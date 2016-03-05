3c85d4ae935f52e1e53e1b82d04f172cd3dc5a5d
blond <andrewblond@yandex.com>
2016-03-05 23:08:18 +0300

    Added best practies

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const errors = require('./lib/possible-errors');
+const errors    = require('./lib/possible-errors');
+const practices = require('./lib/best-practices');
 const variables = require('./lib/variables');
 
 module.exports = {
-    rules: Object.assign(errors, variables)
+    rules: Object.assign(errors, practices, variables)
 };
