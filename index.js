9baed3645e681fa82b7dc731f061718734738d68
blond <andrewblond@yandex.com>
2016-04-08 21:46:06 +0300

    rules: add es6 rules

--- index.js
+++ index.js
@@ -78,6 +78,18 @@ module.exports = {
 
         'no-new-require': 'error',
 
+        /* ECMAScript 6 ======================================================================== */
+        /* http://eslint.org/docs/rules/#ecmascript-6
+        /* ===================================================================================== */
+
+        'constructor-super': 'error',
+        'no-this-before-super': 'error',
+        'no-dupe-class-members': 'error',
+        'no-class-assign': 'error',
+        'no-const-assign': 'error',
+        'no-new-symbol': 'error',
+        'require-yield': 'error',
+
         /* Best Practices ====================================================================== */
         /* http://eslint.org/docs/rules/#best-practices                                          */
         /* ===================================================================================== */
