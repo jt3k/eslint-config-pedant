904bc9442155da671fe48fccac5117de81887515
blond <andrewblond@yandex.com>
2016-04-08 20:19:21 +0300

    examples: fix `comma-dangle` rule

--- examples/bad/extra-characters.js
+++ examples/bad/extra-characters.js
@@ -1,4 +1,4 @@
-/*eslint comma-dangle: ["warn", "never"]*/
+/*eslint comma-dangle: ["error", "never"]*/
 var obj = {
     bar: 'baz',
     qux: 'quux', // IE8 (when not in IE8 document mode) and below will throw an error when
