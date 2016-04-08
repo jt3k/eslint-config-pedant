9cfe57c502e2eff04f2ce2933087f589e83aea0c
blond <andrewblond@yandex.com>
2016-04-08 20:55:07 +0300

    examples: add example for `no-new-requir` rule

---
+++ examples/bad/commonjs.js
@@ -0,0 +1,3 @@
+/*eslint no-new-require: "error"*/
+var instance = new require('class'); // The `instance` variable will be equal to constructor function
+                                     // instead of expected instance of class.
