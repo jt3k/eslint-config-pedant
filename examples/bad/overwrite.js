75f4c00867b0174207c702f51371dfb25389cc70
blond <andrewblond@yandex.com>
2016-05-05 00:55:19 +0300

    docs: add example for `no-unsafe-finally` rule

--- examples/bad/overwrite.js
+++ examples/bad/overwrite.js
@@ -8,3 +8,14 @@ try {
 } catch (e) {
     e = 10; // Overwrite the reference to the error.
 }
+
+/*eslint no-unsafe-finally: "error"*/
+(() => {
+    try {
+        return 1;  // 1 is returned but suspended until finally block ends
+    } catch(err) {
+        return 2;
+    } finally {
+        return 3;  // 3 is returned before 1, which we did not expect
+    }
+})(); // We expect this function to return 1, but result is 3.
