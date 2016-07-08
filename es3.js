7ded85c89f15e2c9862a7be798c6e2ed01ed2355
blond <andrewblond@yandex.com>
2016-07-08 22:14:04 +0300

    chore(node): drop support for Node.js < 4

--- es3.js
+++ es3.js
@@ -1,10 +1,8 @@
 'use strict';
 
-var assign = require('es6-object-assign').assign;
-
-var commonRules = require('./lib/common-rules');
-var es3Rules = require('./lib/es3-rules');
+const commonRules = require('./lib/common-rules');
+const es3Rules = require('./lib/es3-rules');
 
 module.exports = {
-    rules: assign({}, commonRules, es3Rules)
+    rules: Object.assign({}, commonRules, es3Rules)
 };
--- es6.js
+++ es6.js
@@ -1,10 +1,8 @@
 'use strict';
 
-var assign = require('es6-object-assign').assign;
-
-var commonRules = require('./lib/common-rules');
-var es6Rules = require('./lib/es6-rules');
+const commonRules = require('./lib/common-rules');
+const es6Rules = require('./lib/es6-rules');
 
 module.exports = {
-    rules: assign({}, commonRules, es6Rules)
+    rules: Object.assign({}, commonRules, es6Rules)
 };
