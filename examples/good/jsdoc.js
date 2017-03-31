2bf34de13ee87cf1bd115a3472f634245e48b2d8
blond <andrewblond@yandex.com>
2017-04-01 01:17:26 +0300

    fix(valid-jsdoc): turn off `requireParamDescription` option
    For some parameters, such as callback or options the description is
    superfluous.

--- examples/good/jsdoc.js
+++ examples/good/jsdoc.js
@@ -1,11 +1,13 @@
 /*eslint valid-jsdoc: [2, { requireReturn: false, requireReturnDescription: false }]*/
 
 /**
- * Adds two numbers together.
- * @param {int} num1 The first number.
- * @param {int} num2 The second number.
- * @returns {int}.
+ * Evals file contents.
+ *
+ * @param {string} file The filename to eval.
+ * @param {Object} options The eval options.
+ * @param {Function} callback
+ * @returns {Object}
  */
-function foo(num1, num2) {
-    return num1 + num2;
+function fileEval(file, options, callback) {
+   /* ... */
 }
